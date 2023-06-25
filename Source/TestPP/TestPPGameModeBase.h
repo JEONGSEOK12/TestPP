87736d06e8506838e27ee09b254f5ca3eb2e6aa2
OH JEONG SEOK <ojs4872@naver.com>
2023-06-26 05:58:53 +0900

    초기 커밋.

---
+++ Source/TestPP/TestPPGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TestPPGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTPP_API ATestPPGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
